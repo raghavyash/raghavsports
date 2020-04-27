b93852909730673cab4182485ed487a9e716dedc
raghav <raghvendra.singh101@gmail.com>
2020-04-27 15:39:56 +0530

    remove space

--- eqsportsgit/src/main/java/com/raghav/MainApplication.java
+++ eqsportsgit/src/main/java/com/raghav/MainApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
-
 	public static void main(String[] args) {
 		//raghav
 		SpringApplication.run(MainApplication.class, args);
 
 	}
 
+	
 }
